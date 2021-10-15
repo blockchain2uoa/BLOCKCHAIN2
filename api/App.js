092ae7d7c77357cf2eb757ef51d3df395bf2e25e
TonyynoT <tonynguyen0403@gmail.com>
2021-10-15 13:00:54 +1030

    updated production build

--- api/App.js
+++ api/App.js
@@ -3,10 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// const bodyParser = require('body-parser');
 
 // Install cors for cross-origin response
 var cors = require("cors");
-
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +18,13 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+
+// // Configure the bodyParser middleware
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
 
 // cross-origin response 
 app.use(cors());
@@ -32,6 +39,15 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/testAPI", testAPIRouter);
 app.use("/testDB", testDBRouter);
+
+// This middleware informs the express application to serve our compiled React files
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function (req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  }); 
+};
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
