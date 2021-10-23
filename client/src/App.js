0cb4f77aab63435d49a3bcda77252d8459f5076f
Kit <ksaisaard@outlook.com>
2021-10-23 14:28:26 +1030

    updated API address to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   };
   
   callAPI() {
-      fetch("http://localhost:9000/testAPI")
+      fetch("https://blockchain2.herokuapp.com/testAPI")
           .then(res => res.text())
           .then(res => this.setState({ apiResponse: res }));
   };
